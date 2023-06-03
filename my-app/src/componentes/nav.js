5746c81c199a7220c2467c343f4ec18bc07aad43
WalterKc <trabajosham@gmail.com>
2023-06-03 20:20:33 -0300

    Boton de login(pasivo)

--- my-app/src/componentes/nav.js
+++ my-app/src/componentes/nav.js
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 
 import logo from "../imagenes/logotipo renta.png";
 import "./nav.css";
-
+/**
+ * ok, ahora que tenemos algo mas limpio esto, vamos a hacer lo que falta
+ * tenemos que muestre el login, para eso, vamos a hacer 2 cosas
+ * primero, vamos a poner la imagen de login(la persona) y las letras de login
+ * estas van a tener un link, para cuando NO SE ESTE LOGEADO, se pueda logear
+ * segundo, cuando se este logeado, este boton se desactivara, y
+ * aparecera el nombre de el user y si es admin, que es admin
+ */
 export function Nav(estado) {
   const estadoActual = estado.estado;
   const setEstado = estado.set;
@@ -14,6 +21,13 @@ export function Nav(estado) {
       </a>
 
       <div id="menu">
+        <div id="login">
+          <a href="/Login" id="linkLogin">
+            <i class="bi-person-circle " id="iconoLogin"></i>
+          </a>
+          <p id="texto">Login</p>
+        </div>
+
         <button
           onClick={() => setEstado(!estadoActual)}
           class="navbar-toggler collapsed"
